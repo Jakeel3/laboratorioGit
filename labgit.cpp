a8d9b904781d83908653d958b844956b0e448dbf
jrussim-dot <jrussim@unal.edu.co>
2026-05-06 08:40:46 -0500

    Se agrego lineas de codigo para leer el archivo de texto

--- labgit.cpp
+++ labgit.cpp
@@ -1,5 +1,17 @@
 #include<iostream>
 using namespace std;
 int main(){
+	ifstream archivoEntrada("input/archivoEntrada.txt");
+	ofstream archivoSalida("output/archivoSalida.txt");
+	
+	if(archivoEntrada.is_open() 66 archivoSalida.is_open()){
+		cout<<"Archivo  abierto"<<endl;
+		string linea;
+		
+		while (getline(archivoEntrada, linea)){
+			archivoSalida <<linea;
+		}
+		cout<<"Archivo copiado correctamente"<<endl;
+	}
 	
 }
